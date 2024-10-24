3864c409b0eb9c82767ee5757255c6f885fb4a12
fedoseevm <maksim.fedoseev122@gmail.com>
2024-10-24 23:40:06 +0200

    Get the game up and running

--- script.js
+++ script.js
@@ -23,11 +23,9 @@ $(document).ready(function() {
     ];
     
     let selectedCategory = categories[Math.floor(Math.random() * categories.length)];
-    $('.hint').text(`Kategoria: ${selectedCategory.category}`);
+    $('.hint').html(`Kategoria: <span class="roboto-bold">${selectedCategory.category}</span>`);
     
     let selectedWord = selectedCategory.words[Math.floor(Math.random() * selectedCategory.words.length)];
-    // let selectedWord = "don kichot";
-    // alert(selectedWord);
     let guessedLetters = [];
     let mistakes = 0;
     const maxMistakes = 6;
@@ -41,7 +39,7 @@ $(document).ready(function() {
     ];    
     
     function updateHangman() {
-        if (mistakes > 0 && mistakes <= hangmanParts.length) {
+        if (mistakes <= hangmanParts.length) {
             $(hangmanParts[mistakes - 1]).css('display', 'block');
         }
     }
@@ -54,27 +52,26 @@ $(document).ready(function() {
         ];
     
         keyboardLayout.forEach(row => {
-            const keyboardRow = $('<div></div>'); // Tworzymy nowy div dla każdego rzędu
+            const keyboardRow = $('<div class="keyboard-row"></div>');
             row.split(' ').forEach(letter => {
-                keyboardRow.append(`<button>${letter.toUpperCase()}</button>`); // Dodajemy przyciski do rzędu
+                keyboardRow.append(`<button>${letter.toUpperCase()}</button>`); // append(element) dodaje element wewnątrz kontenera na końcu
             });
-            $('#letters-container').append(keyboardRow); // Dodajemy rząd do kontenera
+            $('#letters-container').append(keyboardRow);
+            $('.keyboard-row').css({"display":"inline-flex", "gap":"1.5vw", "justify-content":"center"});
         });
     }
     
     
     function updateWordDisplay() {
-        let displayWord = selectedWord.split(' ').map(word => {
-            // Przetwarzanie każdego słowa
+        let displayWord = selectedWord.split(' ').map(word => { 
             let displayLetters = word.split('').map(letter => {
                 return `<span class="letter" style="width: 1ch; text-align: center;">${guessedLetters.includes(letter) ? letter : "_"}</span>`;
-            }).join(''); // Nie dołączamy spacji między literami
+            }).join(''); 
     
-            // Każde słowo zwraca się otoczone przez span
-            return `<span class="word" style="display: inline-flex; gap: 1.5vw;">${displayLetters}</span>`;
-        }).join(' '); // Odstęp między słowami
+            return `<span class="word" style="display: inline-flex; gap: 1.2vw;">${displayLetters}</span>`;
+        }).join(' '); 
     
-        $('#word-container').html(displayWord.toUpperCase()); // Używamy .html(), aby dodać tagi
+        $('#word-container').html(displayWord.toUpperCase()); //  .html() dodaje tagi
     }
     
     function resetHangman() {
@@ -85,13 +82,13 @@ $(document).ready(function() {
 
     function checkGameStatus() {
         if (mistakes >= maxMistakes) {
-            $('#message').text(`Przegrałeś! Słowo to: ${selectedWord}`);
+            $('#message').html(`Przegrałeś! Słowo to: <span class="roboto-bold">${selectedWord.toUpperCase()}</span>`);
             $('#letters-container').off('click', 'button');
-            $('#restart-game').show();
+            $('#restart-game').css('display','inline-flex');
         } else if (!$('#word-container').text().includes('_')) {
             $('#message').text('Wygrałeś!');
             $('#letters-container').off('click', 'button');
-            $('#restart-game').show();
+            $('#restart-game').css('display','inline-flex');
         }
     }
     
@@ -103,20 +100,40 @@ $(document).ready(function() {
         $('#message').empty();
         $('#letters-container').empty();
         resetHangman();
-        $(this).hide();
+        $(this).css('display','none');
         updateWordDisplay();
         createLetterButtons();
-        $('.hint').text(`Kategoria: ${selectedCategory.category}`);
+        $('.hint').html(`Kategoria: <span class="roboto-bold">${selectedCategory.category}</span>`);
         $('#letters-container').on('click', 'button', function() {
             let letter = $(this).text().toLowerCase();
             $(this).prop('disabled', true);
+        
             if (selectedWord.includes(letter)) {
+                // Ustawienie stylów dla prawidłowej litery
+                $(this).css({
+                    "transform": "translateY(2px)",
+                    'background-color': '#b2d8b2',
+                    'border-color': '#6b8f6b',
+                    'color': '#2f4f2f', 
+                    'box-shadow': '0 2px #6b8f6b, 0 4px 0 2px #4d6b4d',
+                    'cursor': 'default'
+                });
                 guessedLetters.push(letter);
                 updateWordDisplay();
             } else {
+                // Ustawienie stylów dla błędnej litery
+                $(this).css({
+                    "transform": "translateY(2px)",
+                    'background-color': '#e0b2b2', 
+                    'border-color': '#8f6b6b', 
+                    'color': '#4f2f2f',
+                    'box-shadow': '0 2px #8f6b6b, 0 4px 0 2px #6b4d4d',
+                    'cursor': 'default'
+                });
                 mistakes++;
                 updateHangman();
             }
+        
             checkGameStatus();
         });
     });
@@ -124,15 +141,66 @@ $(document).ready(function() {
     $('#letters-container').on('click', 'button', function() {
         let letter = $(this).text().toLowerCase();
         $(this).prop('disabled', true);
+    
         if (selectedWord.includes(letter)) {
+            // Ustawienie stylów dla prawidłowej litery
+            $(this).css({
+                "transform": "translateY(2px)",
+                'background-color': '#b2d8b2', 
+                'border-color': '#6b8f6b', 
+                'color': '#2f4f2f', 
+                'box-shadow': '0 2px #6b8f6b, 0 4px 0 2px #4d6b4d', 
+                'cursor': 'default' 
+            });
             guessedLetters.push(letter);
             updateWordDisplay();
         } else {
+            // Ustawienie stylów dla błędnej litery
+            $(this).css({
+                "transform": "translateY(2px)",
+                'background-color': '#e0b2b2', 
+                'border-color': '#8f6b6b', 
+                'color': '#4f2f2f', 
+                'box-shadow': '0 2px #8f6b6b, 0 4px 0 2px #6b4d4d', 
+                'cursor': 'default' 
+            });
             mistakes++;
             updateHangman();
         }
+    
         checkGameStatus();
     });
+
+    let canPressKey = true; // Flaga kontrolująca opóźnienie
+
+    $(document).on('keydown', function(event) {
+        if (!canPressKey) return; 
+        canPressKey = false; 
+
+        setTimeout(() => {canPressKey = true;}, 500); // setTimeout(code, delay)
+
+        const key = event.key.toLowerCase();
+
+        if (key >= 'a' && key <= 'z') {
+            activateLetterButton(key);
+        } else if (event.code === 'Space') {
+            event.preventDefault(); // Aby nie zachodziła domyślna funkcja spacji
+            activateRestartButton();
+        }
+    });
+
+    function activateLetterButton(letter) {
+        let button = $(`#letters-container button:contains(${letter.toUpperCase()})`);
+        if (button.length && !button.prop('disabled')) {
+            button.click(); 
+        }
+    }
+
+    function activateRestartButton() {
+        if ($('#restart-game').css('display') !== 'none') {
+            $('#restart-game').click();
+        }
+    }
 
     resetHangman();
     updateWordDisplay();
